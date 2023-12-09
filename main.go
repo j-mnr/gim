7186a4a5ef34829d1d5d7550d3d4777bbc57ba3c
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2023-12-09 12:25:54 -0500

    feat: Add Gim struct to hold state

--- main.go
+++ main.go
@@ -9,6 +9,26 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
+type Gim struct {
+	screen tcell.Screen
+	focus *Buffer
+	buffers []*Buffer
+	style tcell.Style
+}
+
+func (g Gim) Draw() {
+		g.screen.Clear()
+
+		for row, line := range g.focus.Window() {
+			for col, r := range line {
+				g.screen.ShowCursor(g.focus.cursor.col, g.focus.cursor.row)
+				g.screen.SetContent(col+5, row, r, nil, g.style)
+			}
+		}
+
+		g.screen.Show()
+}
+
 func main() {
 	s, err := tcell.NewScreen()
 	if err != nil {
@@ -62,24 +82,11 @@ func main() {
 		rtext[i] = bytes.Runes(line)
 	}
 	_, h := s.Size()
-	buf := &Buffer{
-		windowStart: 0,
-		windowEnd:   uint(min(h, len(rtext))),
-		height:      uint(h),
-		fullText:    rtext,
-	}
+	buf := NewBuffer(h, rtext)
+	gim := Gim{screen: s, focus: buf, buffers: []*Buffer{buf}, style: style}
 
 	for {
-		s.Clear()
-
-		for row, line := range buf.fullText[buf.windowStart:buf.windowEnd] {
-			for col, r := range line {
-				s.ShowCursor(buf.cursor.col, buf.cursor.row)
-				s.SetContent(col+5, row, r, nil, style)
-			}
-		}
-
-		s.Show()
+		gim.Draw()
 
 		switch ev := s.PollEvent().(type) {
 		case *tcell.EventKey:
@@ -118,6 +125,20 @@ type Buffer struct {
 	windowEnd   uint
 	height      uint
 	fullText    [][]rune
+}
+
+func NewBuffer(height int, text [][]rune) *Buffer {
+	return &Buffer{
+		cursor: Cursor{col: 5, row: 0},
+		windowStart: 0,
+		windowEnd:   uint(min(height, len(text))),
+		height:      uint(height),
+		fullText:    text,
+	}
+}
+
+func (b Buffer) Window() [][]rune {
+	return b.fullText[b.windowStart:b.windowEnd]
 }
 
 func (b *Buffer) UpdateRow(by int) {
